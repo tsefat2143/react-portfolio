e0ec1b4a3de16cebac93e33670e2320415c44277
tsefat2143 <tajdidul.sefat@gmail.com>
2021-09-12 01:01:20 -0400

    baseUrl

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route path="about" component={About} />
           <Route path="/work-experience" component={Experience} />
           <Route path="/skills" component={Skills} />
           <Route path="/project" component={Project} />
