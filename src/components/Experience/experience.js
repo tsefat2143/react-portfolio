3ecb8d6ebedc965261bf6a00d48fb3c0652ea6f7
tsefat2143 <tajdidul.sefat@gmail.com>
2021-11-27 16:33:06 -0500

    Navbar and Experience

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -20,11 +20,22 @@ const experience = () => {
                 </div>
 
                 <div className='content'>
+                    <h2 className='position'>Web Developer</h2>
+                    <h2 className='workplace'>WireSmart Electrical LLC</h2>
+                    <h3 className='date'>August 2021 - October 2021</h3>
+                    <ul className='task'>
+                        <li>
+                            Created a website to promote client's electrician business.
+                        </li>
+                    </ul>
+                </div>
+
+                <div className='content'>
                     <h2 className='position'>Full Stack Developer</h2>
                     <h2 className='workplace'>
                         <span style={{color:'#0c0c80'}}>Good</span><span style={{color:'orange'}}>MD</span>
                     </h2>
-                    <h3 className='date'>March 2021 - September 2021</h3>
+                    <h3 className='date'>January 2021 - September 2021</h3>
                     <ul className='task'>
                         <li>
                             React & Google Firebase.
