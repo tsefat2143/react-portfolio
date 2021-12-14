22b7eab9042decd9e17bf4769c6e085e52229256
tsefat2143 <tajdidul.sefat@gmail.com>
2021-12-14 06:05:16 -0500

    add link

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -21,7 +21,12 @@ const experience = () => {
 
                 <div className='content'>
                     <h2 className='position'>Web Developer</h2>
-                    <h2 className='workplace'>WireSmart Electrical LLC</h2>
+                    <h2 className='workplace'>
+                        <a href='https://www.wiresmartelectrical.com/'
+                        target='_blank' rel="noreferrer">
+                            WireSmart Electrical LLC
+                        </a>
+                    </h2>
                     <h3 className='date'>August 2021 - October 2021</h3>
                     <ul className='task'>
                         <li>
