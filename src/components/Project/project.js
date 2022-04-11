901d45b6e10abe8544bdbf3769a0f36c2aba1dba
tsefat2143 <tajdidul.sefat@gmail.com>
2022-04-11 15:21:00 -0400

    Project code and Live

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Twitter from './twitter.png'
 import Graduation from './graduation.jpg'
 import Volcano from './volcano.png'
 import Wiresmart from './wiresmart.png'
@@ -14,45 +13,59 @@ const project = () => {
             <h1>PROJECTS I'VE BUILT</h1>
             <div className='project-container'>
                 <div className='project'>
-                    <a href='https://sneaker-store-development.myshopify.com/'
-                        target='_blank' rel="noreferrer">
-                        <img src={Sneaker} alt='Sneaker' />
-                        <p>Sneaker Store Development</p>
-                    </a>
+                    <img src={Sneaker} alt='Sneaker' />
+                    <div className='square'>
+                        <p>Password:rteimp</p>
+                        <p>
+                            <a href='https://sneaker-store-development.myshopify.com/'
+                            target='_blank' rel="noreferrer">
+                                See Live
+                            </a>
+                        </p>
+                    </div>
+                    <p>Sneaker Store Development</p>    
                 </div>
                 <div className='project'>
-                    <a href='https://fitness917475285.wordpress.com/'
-                        target='_blank' rel="noreferrer">
-                        <img src={Fitness} alt='Fitness' />
-                        <p>Fitness Blog</p>
-                    </a>
+                    <img src={Fitness} alt='Fitness' />
+                    <div className='square'>
+                        <p>
+                            <a href='https://fitness917475285.wordpress.com/'
+                            target='_blank' rel="noreferrer">
+                                See Live
+                            </a>
+                        </p>
+                    </div>
+                    <p>Fitness Blog</p>
                 </div>
                 <div className='project'>
-                    <a href='https://www.wiresmartelectrical.com/'
-                        target='_blank' rel="noreferrer">
-                        <img src={Wiresmart} alt='Wiresmart' />
-                        <p>WireSmart Electrical</p>
-                    </a>
+                    <img src={Wiresmart} alt='Wiresmart' />
+                    <div className='square'>
+                        <p>
+                            <a href='https://www.wiresmartelectrical.com/'
+                            target='_blank' rel="noreferrer">
+                                See Live
+                            </a>
+                        </p>
+                    </div>
+                    <p>WireSmart Electrical</p>
                 </div>
                 <div className='project'>
-                    <a href='https://github.com/tsefat2143/Graduation-Form'
-                        target='_blank' rel="noreferrer">
-                        <img src={Graduation} alt='Graduation' />
-                        <p>Graduation</p>
-                    </a>
+                    <img src={Graduation} alt='Graduation' />
+                    <div className='square'>
+                        <p>
+                            <a href='https://github.com/tsefat2143/Graduation-Form'
+                            target='_blank' rel="noreferrer">
+                                See Code
+                            </a>
+                        </p>
+                    </div>
+                    <p>Graduation</p>
                 </div>
                {/*  <div className='project'>
                     <a href='https://github.com/tsefat2143/Volcano-Tracker'
                         target='_blank' rel="noreferrer">
                         <img src={Volcano} alt='Volcano' />
                         <p>Volcano Tracker</p>
-                    </a>
-                </div> */}
-                {/* <div className='project'>
-                    <a href='https://github.com/tsefat2143/SimpleTweet'
-                        target='_blank' rel="noreferrer">
-                        <img src={Twitter} alt='Twitter' />
-                        <p>Simple Tweet</p>
                     </a>
                 </div> */}
 {/*                 <div className='project'>
@@ -63,15 +76,22 @@ const project = () => {
                     </a>
                 </div> */}
                 <div className='project'>
-                    <a href='https://github.com/tsefat2143/Car-Slideshow-Landing-Page'
-                        target='_blank' rel="noreferrer">
-                        <img src={Cars} alt='Cars' />
-                        <div className='square'>
-                            <p>See Code</p>
-                            <p>See Live</p>
-                        </div>
-                        <p>Cars Landing Page</p>
-                    </a>
+                    <img src={Cars} alt='Cars' />
+                    <div className='square'>
+                        <p>
+                            <a href='https://github.com/tsefat2143/Car-Slideshow-Landing-Page'
+                            target='_blank' rel="noreferrer">
+                                See Code
+                            </a>
+                        </p>
+                        <p>
+                            <a href='https://car-slideshow.netlify.app/'
+                            target='_blank' rel="noreferrer">
+                                See Live
+                            </a>
+                        </p>
+                    </div>
+                    <p>Cars Landing Page</p>
                 </div>
             </div>
         </div>
