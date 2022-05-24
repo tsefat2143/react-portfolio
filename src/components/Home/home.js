7bbdcc720f5b88a36499d912190f5442d1e48544
tsefat2143 <tajdidul.sefat@gmail.com>
2022-05-24 11:03:54 -0400

    Modify animations

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -4,15 +4,24 @@ import React, { useEffect } from 'react'
 import Socials from '../Socials/socials'
 import Typewriter from "typewriter-effect";
 import beach from '../../beach.mp4'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './home.css'
 
 const Home = () => {
+    useEffect(() => {
+        AOS.init({
+            duration:2000,
+            once: true
+        });
+    }, []);
+
     return (
         <div className='home' id='/'>
             <video autoPlay loop muted>
                 <source src={beach}></source>
             </video>
-            <div className='title'>
+            <div data-aos='slide-right' className='title'>
                 <h1>
                     <p>Hi,</p>
                     <p>I am Tajdidul Sefat,</p>
