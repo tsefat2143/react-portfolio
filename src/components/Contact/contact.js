ea4666563f54444a9770dc517bb43860cc9e7b49
tsefat2143 <tajdidul.sefat@gmail.com>
2022-05-24 09:56:46 -0400

    Modify animations

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,17 +1,7 @@
-import React, {useEffect} from 'react'
 import emailJS from 'emailjs-com'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import './contact.css'
 
-const Contact = () => {
-    useEffect(() => {
-        AOS.init({
-            duration:2000,
-            once: true
-        });
-    }, []);
-    
+const Contact = () => {    
     function sendEmail(e){
         e.preventDefault();
 
@@ -25,7 +15,7 @@ const Contact = () => {
     }
 
     return (
-        <div data-aos='fade-down' className='contact-div' id='contact'>
+        <div className='contact-div' id='contact'>
             <h1>CONTACT ME</h1>
             <form onSubmit={sendEmail}>
                 <div className="form-group">
--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -1,35 +1,10 @@
-import React, { useEffect } from 'react'
-//import logo from './logo.jpg'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import './experience.css'
 
 const Experience = () => {
-    useEffect(() => {
-        AOS.init({
-            duration:2000,
-            once: true
-        });
-    }, []);
-
     return (
         <div className='work-experience' id='work-experience'>
             <h1>RELEVANT EXPERIENCE</h1>
-{/*             <div className='content'>
-                <h2 className='position'>Sr. Admin Analyst</h2>
-                    <a href='https://www1.nyc.gov/site/hra/about/about-hra.page'
-                    target='_blank' rel="noreferrer">
-                        <img src={logo} height='80px' alt='hra-logo' />
-                    </a>
-                    <h3 className='date'>August 2021 - On Going</h3>
-                    <ul className='task'>
-                        <li>
-                            Utilized company software to upload and retrieve client information from various databases.
-                        </li>
-                    </ul>
-                </div> */}
-
-                <div data-aos='fade-right' className='content'>
+                <div className='content'>
                     <h2 className='position'>Web Developer</h2>
                     <h2 className='workplace'>
                         <a href='https://www.wiresmartelectrical.com/'
@@ -45,7 +20,7 @@ const Experience = () => {
                     </ul>
                 </div>
 
-                <div data-aos='fade-right' className='content'>
+                <div className='content'>
                     <h2 className='position'>Full Stack Developer</h2>
                     <h2 className='workplace'>
                         <span style={{color:'#0c0c80'}}>Good</span><span style={{color:'orange'}}>MD</span>
