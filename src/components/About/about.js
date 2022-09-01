2c26ed9f5709428eeaea9bc5876f44674ab2a1cb
tsefat2143 <tajdidul.sefat@gmail.com>
2022-08-31 20:42:23 -0400

    fix grammar

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -16,15 +16,14 @@ const About = () => {
             <h1>ABOUT ME</h1>
             <p>
                 Hi Everyone! My name is Tajdidul. 
-                By using various programming languages I like to create 
+                By using various programming languages, I like to create 
                 projects and learn new technologies to better my skills. 
-                I have graduated from the City University 
-                of New York at Queens College and plan to enjoy the ride 
-                of web development. During my weekends, I love to go
+                I graduated from the City University of New York at Queens College 
+                and plan to enjoy the journey of web development. During my weekends, I love to go
                 to the gym, play sports, or watch a comedy show.
             </p>
             <p>
-                I am currently a Analyst Quality Assurance at TCS - PNC Bank. 
+                I am currently an Analyst Quality Assurance at TCS - PNC Bank. 
                 I am open to other roles within the United States, so please feel free to 
                 <a href='/#contact'> contact </a> me.
             </p>
