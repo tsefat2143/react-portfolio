31144ed99a7b154019745f930d2a18ab0d9034a1
tsefat2143 <tajdidul.sefat@gmail.com>
2023-05-20 07:44:32 -0400

    add photo

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -2,6 +2,7 @@ import Aos from 'aos'
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import './about.css'
+import Me from './me.jpg'
 
 const About = () => {
     useEffect(() => {
@@ -12,21 +13,26 @@ const About = () => {
     })
 
     return (
-        <div data-aos='fade-down' className='about' id='about'>
-            <h1>ABOUT ME</h1>
-            <p>
-                Hi Everyone! My name is Tajdidul. 
-                By using various programming languages, I like to create 
-                projects and learn new technologies to better my skills. 
-                I graduated from the City University of New York at Queens College 
-                and plan to enjoy the journey of web development. During my free time, I love to go
-                to the gym, play sports, go snowboarding, or watch a comedy show.
-            </p>
-            <p>
-                I am currently an Analyst Quality Assurance at PNC Financial Services. 
-                I am open to other roles within the United States, so please feel free to 
-                <a href='/#contact'> contact </a> me.
-            </p>
+        <div className='about' id='about'>
+            <h1 data-aos='fade-down'>ABOUT ME</h1>
+            <div className='about-div'>
+                <img src={Me} alt='Me' data-aos={window.innerWidth>=1300 ? 'fade-right': 'fade-down'} />
+                <div className='bio' data-aos={window.innerWidth>=1300 ? 'fade-left': 'fade-down'}>
+                    <p>
+                        Hi Everyone! My name is Tajdidul. 
+                        By using various programming languages, I like to create 
+                        projects and learn new technologies to better my skills. 
+                        I graduated from the City University of New York at Queens College 
+                        and plan to enjoy the journey of web development. During my free time, I love to go
+                        to the gym, play sports, go snowboarding, or watch a comedy show.
+                    </p>
+                    <p>
+                        I am currently an Analyst Quality Assurance at PNC Financial Services. 
+                        I am open to other roles within the United States, so please feel free to 
+                        <a href='/#contact'> contact </a> me.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
