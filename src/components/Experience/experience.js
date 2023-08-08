52f7d7110ca360f526888007ebda35402ddc1866
tsefat2143 <tajdidul.sefat@gmail.com>
2023-08-08 11:00:59 -0400

    add experience

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -15,16 +15,28 @@ const Experience = () => {
         <div className='work-experience' id='work-experience'>
             <h1 data-aos='fade-down'>RELEVANT EXPERIENCE</h1>
 
-                {/* <div data-aos={window.innerWidth>=860 ? 'fade-left': 'fade-down'} className='content'>
+                <div data-aos={window.innerWidth>=860 ? 'fade-left': 'fade-down'} className='content'>
                     <h2 className='position'>Web Developer</h2>
-                    <h2 className='workplace'>Power Smart Electric</h2>
-                    <h3 className='date'>July 2022 - September 2022</h3>
+                    <h2 className='workplace'>Power Smart Electric LLC</h2>
+                    <h3 className='date'>December 2022 - August 2023</h3>
                     <ul className='task'>
                         <li>HTML, CSS, JavaScript, jQuery, AJAX, SQL, PHP</li>
-                        <li>Hosted with Heroku.</li>
+                        <li>Hosted with Hostinger.</li>
                         <li>Created a website to promote client's electrician business.</li>
+                        <li>Created a rating system in the backend so customers can leave a review of the company's services.</li>
                     </ul>
-                </div> */}
+                </div>
+
+                <div data-aos={window.innerWidth>=860 ? 'fade-left': 'fade-down'} className='content'>
+                    <h2 className='position'>Analyst Quality Assurance</h2>
+                    <h2 className='workplace'>PNC Financial Services</h2>
+                    <h3 className='date'>July 2022 - February 2023</h3>
+                    <ul className='task'>
+                        <li>SQL, Python</li>
+                        <li>Wrote complex SQL queries to test different types of business logic in congruence to PNC's and their client's data</li>
+                        <li>Used Python to automate test cases.</li>
+                    </ul>
+                </div>
 
                 <div data-aos={window.innerWidth>=860 ? 'fade-left': 'fade-down'} className='content'>
                     <h2 className='position'>Web Developer</h2>
